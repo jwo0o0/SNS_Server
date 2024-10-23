863f4efe525febac54bdb9fa75a2e6456f6423c6
jwo0o0 <jwo0o0358@gmail.com>
2024-10-23 20:37:23 +0900

    fix: cors 도메인 추가

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ if (process.env.NODE_ENV === "production") {
 
 app.use(
   cors({
-    origin: ["http://localhost:3000"], // 클라이언트 도메인을 정확히 지정
+    origin: ["http://localhost:3000", "https://sns.jwoo.site"], // 클라이언트 도메인을 정확히 지정
     credentials: true, // 자격 증명 허용
   })
 );
