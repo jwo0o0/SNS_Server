6d10eca43975858378d45f7c2f2573da6c41a113
jwo0o0 <jwo0o0358@gmail.com>
2024-10-25 01:16:12 +0900

    fix: users 모델명 변경

--- passport/index.js
+++ passport/index.js
@@ -1,6 +1,6 @@
 const passport = require("passport");
 const kakao = require("./kakaoStrategy");
-const User = require("../models/users");
+const Users = require("../models/users");
 
 module.exports = () => {
   passport.serializeUser((user, done) => {
@@ -11,7 +11,7 @@ module.exports = () => {
   // 세션에 저장된 아이디를 통해 사용자 정보 객체 복구
   passport.deserializeUser(async (id, done) => {
     try {
-      const user = await User.findOne({ where: { id } });
+      const user = await Users.findOne({ where: { id } });
       done(null, user); // 사용자 정보를 복구하여 req.user에 저장
     } catch (error) {
       done(error);
